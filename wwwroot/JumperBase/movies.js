bc586d4f90d21e8b5eec539fd48293d1609a5de1
Mike <63655075+MixelTe@users.noreply.github.com>
2020-05-18 13:37:27 +0300

    movies particle from Vlife
    2.4.3

--- wwwroot/JumperBase/movies.js
+++ wwwroot/JumperBase/movies.js
@@ -1,5 +1,5 @@
 import { fileLoaded } from "./loading.js";
-import { switchMovie, WorldAnchor, switchMoveScreen, gameWindowStatic, Screen_edge_right } from "./base.js";
+import { switchMovie, WorldAnchor, switchMoveScreen, gameWindowStatic } from "./base.js";
 import { random_num, rectIntersect } from "./Functions.js";
 import { Particle, drawParticles } from "./particles.js";
 import { get_Vlife } from "./lifeSystem.js";
@@ -7,7 +7,7 @@ import { moveScreenTo } from "./movement.js";
 
 const movie = { done: true, toSavePoint: { boolean: false, counter: 0, changed: false, chr: {}, savePoint: {} } };
 window.movie = movie;
-const pVlife = { particles: [], chr: {}, time: 0, done: true, ending: false };
+const pVlife = { to: { particles: [], chr: {}, time: 0, done: true, ending: false }, from: { particles: [], chr: {}, time: 0, done: true, ending: false }};
 const images = {All: 1, Loaded: 0};
 let Vlife = {}
 const comands = [];
@@ -84,9 +84,13 @@ export function movies()
             moveScreenTo(movie.toSavePoint.chr.x - Vlife.x);
         }
     }
-    if (!pVlife.done)
+    if (!pVlife.to.done)
     {
         particles_to_Vlife();
+    }
+    if (!pVlife.from.done)
+    {
+        particles_from_Vlife();
     }
     checkMoviesDone();
     if (movie.done)
@@ -102,11 +106,16 @@ export function movies()
 }
 export function moviesDraw()
 {
-    if (!pVlife.done)
+    if (!pVlife.to.done)
     {
-        drawParticles(pVlife.particles, true);
+        drawParticles(pVlife.to.particles, true);
+    }
+    if (!pVlife.from.done)
+    {
+        drawParticles(pVlife.from.particles, true);
     }
 }
+
 
 
 function movementTo(obj, fx, fy, sx, sy, speedX, speedY, el)
@@ -136,46 +145,80 @@ function movementTo(obj, fx, fy, sx, sy, speedX, speedY, el)
 
 function particles_command(command)
 {
-    if (command.action == "start")
-    {
-        pVlife.chr = command.chr;
-        pVlife.time = command.time;
-        pVlife.done = false;
-        pVlife.ending = false;
+    switch (command.direction) {
+        case "to":
+            if (command.action == "start")
+            {
+                pVlife.to.chr = command.chr;
+                pVlife.to.time = command.time;
+                pVlife.to.done = false;
+                pVlife.to.ending = false;
 
-        command.done = true;
-    }
-    else if (command.action == "stop")
-    {
-        pVlife.ending = true;
+                command.done = true;
+            }
+            else if (command.action == "stop")
+            {
+                pVlife.to.ending = true;
 
-        command.done = true;
-    }
-    else
-    {
-        console.log("action don't correct. fuction: ", particles_command);
+                command.done = true;
+            }
+            else
+            {
+                console.log("action don't correct. fuction: ", particles_command);
+            }
+            break;
+
+        case "from":
+            if (command.action == "start")
+            {
+                pVlife.from.chr = command.chr;
+                pVlife.from.time = command.time;
+                pVlife.from.done = false;
+                pVlife.from.ending = false;
+
+                command.done = true;
+            }
+            else if (command.action == "stop")
+            {
+                pVlife.from.ending = true;
+
+                command.done = true;
+            }
+            else
+            {
+                console.log("action don't correct. fuction: ", particles_command);
+            }
+            break;
+
+        default:
+            break;
     }
 }
 function particles_to_Vlife()
 {
-    if (!pVlife.ending)
+    if (!pVlife.to.ending)
     {
-        const red = random_num(0, 100);
-        const green = random_num(190, 255);
-        const blue = random_num(0, 100);
-        const newColor = `rgb(${red}, ${green}, ${blue})`;
-        const width = random_num(6, 10);
-        const p = new Particle(pVlife.chr.x + pVlife.chr.width / 2, pVlife.chr.y + pVlife.chr.height / 2, width, width, "ghost", newColor, false);
-        p.ex = Vlife.x - WorldAnchor.x;
-        p.ey = Vlife.y - WorldAnchor.y;
-        p.speedX = (p.ex - p.x) / pVlife.time;
-        p.speedY = (p.ey - p.y) / pVlife.time;
-        p.time = pVlife.time;
-        pVlife.particles.push(p);
+        for (let i = 0; i < 3; i++)
+        {
+            const red = random_num(0, 100);
+            const green = random_num(190, 255);
+            const blue = random_num(0, 100);
+            const newColor = `rgb(${red}, ${green}, ${blue})`;
+            const width = random_num(6, 10);
+            const p = new Particle(0, 0, width, width, "ghost", newColor, false);
+            p.x = pVlife.to.chr.x + pVlife.to.chr.width / 2 + random_num(-30, 30);
+            p.y = pVlife.to.chr.y + pVlife.to.chr.height / 2 + random_num(-30, 30);
+            p.ex = Vlife.x - WorldAnchor.x;
+            p.ey = Vlife.y - WorldAnchor.y;
+            p.speedX = (p.ex - p.x) / pVlife.to.time;
+            p.speedY = (p.ey - p.y) / pVlife.to.time;
+            p.time = pVlife.to.time;
+            pVlife.to.particles.push(p);
+        }
     }
-    for (let i = 0; i < pVlife.particles.length; i++)
+    for (let i = 0; i < pVlife.to.particles.length; i++)
     {
-        const el = pVlife.particles[i];
+        const el = pVlife.to.particles[i];
         el.time = Math.max(el.time - 1, 1);
         el.ex = Vlife.x - WorldAnchor.x;
         el.ey = Vlife.y - WorldAnchor.y;
@@ -184,13 +227,55 @@ function particles_to_Vlife()
         movementTo(el, el.x, el.y, el.ex, el.ey, el.speedX, el.speedY, el);
         if (el.done)
         {
-            pVlife.particles.splice(i, 1);
+            pVlife.to.particles.splice(i, 1);
             i -= 1;
         }
     }
-    if (pVlife.particles.length == 0)
+    if (pVlife.to.particles.length == 0)
     {
-        pVlife.done = true;
+        pVlife.to.done = true;
+    }
+}
+function particles_from_Vlife()
+{
+    if (!pVlife.from.ending)
+    {
+        for (let i = 0; i < 5; i++)
+        {
+            const red = random_num(0, 100);
+            const green = random_num(190, 255);
+            const blue = random_num(0, 100);
+            const newColor = `rgb(${red}, ${green}, ${blue})`;
+            const width = random_num(6, 10);
+            const p = new Particle(0, 0, width, width, "ghost", newColor, false);
+            p.x = Vlife.x - WorldAnchor.x + random_num(-45, 45);
+            p.y = Vlife.y - WorldAnchor.y + random_num(-45, 45);
+            p.ex = pVlife.from.chr.x + pVlife.from.chr.width / 2;
+            p.ey = pVlife.from.chr.y + pVlife.from.chr.height / 2;
+            p.speedX = (p.ex - p.x) / pVlife.from.time;
+            p.speedY = (p.ey - p.y) / pVlife.from.time;
+            p.time = pVlife.from.time;
+            pVlife.from.particles.push(p);
+        }
+    }
+    for (let i = 0; i < pVlife.from.particles.length; i++)
+    {
+        const el = pVlife.from.particles[i];
+        el.time = Math.max(el.time - 1, 1);
+        el.ex = pVlife.from.chr.x + pVlife.from.chr.width / 2;
+        el.ey = pVlife.from.chr.y + pVlife.from.chr.height / 2;
+        el.speedX = (el.ex - el.x) / el.time;
+        el.speedY = (el.ey - el.y) / el.time;
+        movementTo(el, el.x, el.y, el.ex, el.ey, el.speedX, el.speedY, el);
+        if (el.done)
+        {
+            pVlife.from.particles.splice(i, 1);
+            i -= 1;
+        }
+    }
+    if (pVlife.from.particles.length == 0)
+    {
+        pVlife.from.done = true;
     }
 }
 
@@ -212,7 +297,7 @@ function moveTo(fx, fy, x, y, chr, time)
     comands.push(a);
 }
 
-function ptoVlife(action, chr, time)
+function ptofromVlife(action, direction, chr, time)
 {
     if (action == "start")
     {
@@ -220,27 +305,23 @@ function ptoVlife(action, chr, time)
             action: action,
             time: time,
             chr: chr,
+            direction: direction,
             done: false,
             type: "particles",
             way: "continue",
         };
         comands.push(a);
-
-        // pVlife.chr = chr;
-        // pVlife.time = time;
-        // pVlife.done = false;
-        // pVlife.ending = false;
     }
     else if (action == "stop")
     {
         const a = {
             action: action,
+            direction: direction,
             done: false,
             type: "particles",
             way: "continue",
         };
         comands.push(a);
-        // pVlife.ending = true;
     }
     else
     {
@@ -258,7 +339,11 @@ function checkMoviesDone()
             movie.done = false;
         }
     }
-    if (!pVlife.done)
+    if (!pVlife.to.done)
+    {
+        movie.done = false;
+    }
+    if (!pVlife.from.done)
     {
         movie.done = false;
     }
@@ -327,13 +412,16 @@ export function movie_toSavePoint(chr, savePoint)
     changeBoolean(movie.toSavePoint, true)
     stepCounter(movie.toSavePoint)
 
-    ptoVlife("start", chr, 50);
+    ptofromVlife("start", "to", chr, 25);
     moveTo(chr.x, chr.y, x, y, chr, 50);
-    ptoVlife("stop");
+    ptofromVlife("stop", "to");
+    ptofromVlife("start", "from", chr, 25);
 
     stepCounter(movie.toSavePoint)
 
     moveTo(x, y, savePoint.x, savePoint.y, chr, 50);
+    ptofromVlife("stop", "from");
+
     stepCounter(movie.toSavePoint, true)
     changeBoolean(movie.toSavePoint, false)
 
